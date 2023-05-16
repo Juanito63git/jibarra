b9aee8bbe4b3baa2f51db32bec7aaff14437f540
Juan IBARRA <juanibarra63@outlook.com>
2023-05-15 21:13:05 -0300

    deploy

--- src/main/java/com/portfolio/jIbarra/Controller/CEducacion.java
+++ src/main/java/com/portfolio/jIbarra/Controller/CEducacion.java
@@ -23,7 +23,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/educacion")
-@CrossOrigin(origins = "http://localhost:4200")
+@CrossOrigin(origins = "https://ap2023frontend.web.app")
 public class CEducacion {
     @Autowired
     SEducacion sEducacion;
--- src/main/java/com/portfolio/jIbarra/Controller/CHys.java
+++ src/main/java/com/portfolio/jIbarra/Controller/CHys.java
@@ -22,7 +22,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 
 @RestController
-@CrossOrigin(origins = "http://localhost:4200")
+@CrossOrigin(origins = "https://ap2023frontend.web.app")
 @RequestMapping("/skill")
 public class CHys {
 
